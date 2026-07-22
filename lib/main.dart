113e3b092757c783d46b2dce8776c238de6f00f7
Dheeraj <vishal.yadav.dev025@gmail.com>
2026-07-22 13:32:38 +0530

    change app logo

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     ChangeNotifierProvider(
-      create: (_) =>WeatherProvider()..initDefaultWeather(), // 👈 auto call here
+      create: (_) =>WeatherProvider()..initDefaultWeather(),
       child: const MyApp(),
     ),
   );
